2d781c8afce26658f36250e16162fe7aa07dc51a
Hank <github.unlocking468@passmail.net>
2024-02-15 20:59:39 -0500

    Re-organized classes in Footer, and added sky-500 color to anchor links.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,19 @@
-// src/components/Footer.js
-
 import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-wrap flex-col w-full place-items-center items-center bg-black text-background py-3">
+    <footer className="w-full bg-black py-3 flex flex-wrap flex-col items-center place-items-center text-background">
       <p className="font-medium">Law Office of Nicholas K. Holmes, PLLC</p>
       <p className="text-sm font-medium">
-        <a href="tel:16034969204" target="_blank">
+        <a className="text-sky-500" href="tel:16034969204" target="_blank">
           (603) 496-9204
         </a>{" "}
         |{" "}
-        <a href="mailto:nholmes@nb-holmes.com" target="_blank">
+        <a
+          className="text-sky-500"
+          href="mailto:nholmes@nb-holmes.com"
+          target="_blank"
+        >
           nholmes@nb-holmes.com
         </a>
       </p>
